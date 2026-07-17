4ae3b468becc62100ba479398c60172cb59d1799
Ethan Saranza <ethansaranza17@gmail.com>
2026-07-17 11:53:38 +0800

    refactor(auth): complete authentication slice

--- backend/src/main/java/edu/cit/saranza/byahengcebu/features/auth/repository/UserRepository.java
+++ backend/src/main/java/edu/cit/saranza/byahengcebu/features/auth/repository/UserRepository.java
@@ -1,18 +1,12 @@
 package edu.cit.saranza.byahengcebu.features.auth.repository;
 
+import edu.cit.saranza.byahengcebu.features.auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.cit.saranza.byahengcebu.features.auth.entity.User;
-
-
-public interface UserRepository
-        extends JpaRepository<User,Long>{
-
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
-
 
 }
